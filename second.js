ef323a632755c4441b4af599df23f091ff61f5a1
lishuangg <1241982864@qq.com>
2020-07-20 00:13:41 +0000

    publish

---
+++ second.js
@@ -0,0 +1,1 @@
+$(function(){echarts.init(document.getElementById("second")).setOption({title:{text:"JavaScript语言排名变化"},tooltip:{trigger:"axis"},xAxis:{data:["2000","2005","2010","2015","2020"]},yAxis:{},series:[{name:"排名",type:"line",data:["6","9","8","8","7"]}]})});
